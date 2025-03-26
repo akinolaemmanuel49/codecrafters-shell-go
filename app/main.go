a6584508f41bd714be25268b2db38e4c6f3671e5
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2025-03-26 18:06:31 +0100

    REPL implementation

--- app/main.go
+++ app/main.go
@@ -10,15 +10,15 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
-	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprint(os.Stderr, "Error reading input:", err)
-		return
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Error reading input:", err)
+			return
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
