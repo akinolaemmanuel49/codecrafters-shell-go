603f047f7de02e7ea375a4c062743a50dcba0871
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2025-03-26 17:51:43 +0100

    get command from input

--- app/main.go
+++ app/main.go
@@ -14,5 +14,10 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input")
+		return
+	}
+	fmt.Println(command[:len(command)-1], ":command not found")
 }
