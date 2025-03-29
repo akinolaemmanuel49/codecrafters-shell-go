f5977e50c996e31f5fb63b22612b7ba44ac2df39
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2025-03-29 11:35:16 +0100

    Fix backslashes

--- app/parser/parser.go
+++ app/parser/parser.go
@@ -24,38 +24,6 @@ func ParseInput(input string) ([]string, error) {
 		return args, nil
 	}
 
-	for i := 0; i < len(input); i++ {
-		ch := rune(input[i])
-
-		if escaped {
-			current.WriteRune(ch)
-			escaped = false
-			continue
-		}
-
-		switch ch {
-		case '\\':
-			escaped = true
-		case ' ', '\t':
-			if inQuotes != 0 {
-				current.WriteRune(ch) // Keep spaces inside quotes
-			} else if current.Len() > 0 {
-				tokens = append(tokens, current.String())
-				current.Reset()
-			}
-		case '\'', '"':
-			if inQuotes == ch {
-				inQuotes = 0 // Closing quote
-			} else if inQuotes == 0 {
-				inQuotes = ch // Opening quote
-			} else {
-				current.WriteRune(ch) // Allow different nested quotes
-			}
-		default:
-			current.WriteRune(ch)
-		}
-	}
-
 	if escaped {
 		return nil, errors.New("syntax error: trailing backslash")
 	}
